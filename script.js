69d8239ea9b8770ab2447bf1c2dabbd0c3f7ca0c
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2024-12-03 15:20:22 +0200

    added the JS functions and added functionality to the toggle,delete and new button

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
 
-const myLibrary = ["The Way Of The Superior Man", 
-                    "Harry Potter And The Philosophers Stone",
-                    "The Millionare Fast Lane"];
-                    
-const library = document.querySelector(".library")
-const newBook = document.getElementById("new-book")
-
-function Book(name,author,pages,){
-    //the constructor
-    this.name = name;
+const myLibrary = [];
+         
+function Book(title, author, pages, read){
+    this.title = title;
     this.author = author;
     this.pages = pages;
-    this.info - function(){
-        console.log(this.title + "by" + this.author + ", " + this.pages + " pages, ");
+    this.read = read;
+}
+
+Book.prototype.toggleRead = function(){
+    this.read = !this.read;
+}
+
+function toggleRead(index){
+    myLibrary[index].toggleRead();
+    render();
+}
+
+function render(){
+    let libraryEl = document.querySelector(".library");
+    libraryEl.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i];
+        let bookEl = document.createElement("div");
+        bookEl.innerHTML = `
+        <p class="title">Title: ${book.title}</p>
+        <p class="author">By: ${book.author}</p>
+        <p class="pages">Pages: ${book.pages}</p>
+        <p class=read-status">Status: ${book.read ? "Read" : "Not Read Yet"}</p>
+        <button class="delete-book" onclick="removeBook(${i})">Delete</button>
+        <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+        `
+        libraryEl.appendChild(bookEl);
     }
+}
 
+function removeBook(index){
+    myLibrary.splice(index, 1);
+    render();
 }
 
 
-function addBookToLibrary(book){
-    this.book = book;
-    myLibrary.push(book);
-
+function addBookToLibrary(){
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#page_numbers").value;
+    let read = document.querySelector("#read_or_not").value;
+    let newBook = new Book(title, author,pages, read);
+    console.log(newBook);
+    myLibrary.push(newBook);
+    render();
 }
 
-//create the new book button and act
+const newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function(){
+    const newBookForm = document.querySelector("#new-book-form")
+    newBookForm.style.display = "flex";
+})
 
-
-
-function displayBook(){
-    for (i = 0; i < myLibrary.length; i++){
-        const books = document.createElement("div")
-        books.className = 'book-selection';
-        books.textContent = myLibrary[i];
-        const deleteBook = document.createElement("button")
-        deleteBook.textContent = "Delete Book";
-        deleteBook.className = 'delete-book';
-        deleteBook.addEventListener("click", function(e){
-            myLibrary.slice(myLibrary[i]);
-        });
-        library.append(books);
-        books.append(deleteBook);
-
-        //add the read status button
-    }
-
-}
-
-displayBook();
-
-
-
+document.querySelector("#new-book-form").addEventListener("submit",function(event){
+    event.preventDefault();
+    alert("hello world");
+    addBookToLibrary();
+})
